749e1bc6b0a0563da08306c553e43f522cb5a531
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-06-30 20:56:40 +0500

    created a class and constructor in oop

---
+++ oop.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self):
+        self.name = "Ibrar Ahmed"
+        self.age = 30
+
+student1 = Student()
+student2 = Student()
+
+print(student1.name)
+print(student2.age)
