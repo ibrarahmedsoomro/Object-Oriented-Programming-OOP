92ba19c8e4f91b31374cb3511211aaa01e39ed95
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-07-09 21:03:37 +0500

    practice on getter setter with another concept

--- getterSetter.py
+++ getterSetter.py
@@ -1,20 +1,34 @@
-class Student:
+class CommercialBank:
     def __init__(self, name):
         self.__name = name
     
     def get_name(self):
-        return  self.__name
+        return self.__name
     
-    def set_name(self, new_name):
-        if new_name !=  "":
-            self.__name = new_name
-        else:
-            print("Invalid Name")
+    def set_name(self, newName):
+        self.__name = newName
+bankName = CommercialBank("UBL")
+bankName.set_name("ABL")
+print(bankName.get_name())
 
-student = Student("Ibrar Ahmed")
-student.set_name("Ahmed")
 
-print(student.get_name())
+# class Student:
+#     def __init__(self, name):
+#         self.__name = name
+    
+#     def get_name(self):
+#         return  self.__name
+    
+#     def set_name(self, new_name):
+#         if new_name !=  "":
+#             self.__name = new_name
+#         else:
+#             print("Invalid Name")
+
+# student = Student("Ibrar Ahmed")
+# student.set_name("Ahmed")
+
+# print(student.get_name())
 # class Student:
 #     def __init__(self, name):
 #         self.__name = name
