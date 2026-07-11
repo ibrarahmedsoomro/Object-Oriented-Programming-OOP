059d420ce003fe89a491486eafd161820d9ef13e
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-07-12 00:31:16 +0500

    working in inheritance

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Suzuki:
+    def mehran(self):
+        print("Hello mehran")
+
+class KIA:
+    pass
+cars = Suzuki()
+cars.mehran()
+
+# class Person:
+#     def speak(self):
+#         print("Person is Speaking Parrent  Class")
+    
+# class Student:
+#     def shagrid(Person):
+#         print("Child Class")
+
+# student1 = Student()
+# student1.speak()
