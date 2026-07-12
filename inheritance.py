52a18d46d314f14a84bd42acdc25d6b66e9b717c
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-07-12 20:36:50 +0500

    working on inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,19 +1,55 @@
-class Suzuki:
-    def mehran(self):
-        print("Hello mehran")
+# Inheritance with Super Method
+class Mobiles:
+    def __init__(self, name):
+        self.name = name
 
-class KIA:
-    pass
-cars = Suzuki()
-cars.mehran()
+class Xiaomi(Mobiles):
+    def __init__(self, name, functionality):
+        super().__init__(name)
+        self.functionality = functionality
 
-# class Person:
-#     def speak(self):
-#         print("Person is Speaking Parrent  Class")
+laptop = Xiaomi("Redmi", "Turbo")
+print(laptop.name)
+print(laptop.functionality)
+# class ProProgrammer(Programmer):
+#      pass
+
+# Bignner = Programmer("Ibrar")
+# print(Bignner.name)
+
+
+
+
+
+
+                # Inheritance
+
+# class Programmer:
+#     def __init__(self, name):
+#          self.name = name
+
+# class ProProgrammer(Programmer):
+#      pass
+
+# Bignner = Programmer("Ibrar")
+# print(Bignner.name)
+
+# class Suzuki:
+#     def mehran(self):
+#         print("Hello mehran")
+
+# class KIA:
+#     pass
+# cars = Suzuki()
+# cars.mehran()
+
+# # class Person:
+# #     def speak(self):
+# #         print("Person is Speaking Parrent  Class")
     
-# class Student:
-#     def shagrid(Person):
-#         print("Child Class")
+# # class Student:
+# #     def shagrid(Person):
+# #         print("Child Class")
 
-# student1 = Student()
-# student1.speak()
+# # student1 = Student()
+# # student1.speak()
