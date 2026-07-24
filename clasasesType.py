68050a8fdd508a6f464a90cd3e18157500688c8c
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2026-07-24 21:28:10 +0500

    practicing in classes

---
+++ clasasesType.py
@@ -0,0 +1,19 @@
+class Student:
+    name = "Ibrar"
+    age = 30
+
+    def display(self):
+      print(f"Name is {self.name} \nage is {self.age}")
+
+std = Student()
+std.display()
+
+# Normal Class In Python OOP
+
+# class Student:
+#     name = "Ibrar Ahmed"
+#     age = 30
+
+# std = Student()
+# print(std.name)
+# print(std.age)
